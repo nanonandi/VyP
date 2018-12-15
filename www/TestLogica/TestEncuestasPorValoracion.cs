352a4969f10d8bfb6c1954dd8f62a8f13c04b0e2
nanonandi <nanonandi@outlook.com>
2018-12-15 21:24:42 +0100

    datatable

---
+++ www/TestLogica/TestEncuestasPorValoracion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logica;
+
+namespace TestLogica
+{
+    /// <summary>
+    /// Descripción resumida de TestEncuestasPorValoracion
+    /// </summary>
+    [TestClass]
+    public class TestEncuestasPorValoracion
+    {
+        BaseDatos b = new BaseDatos();
+
+        [TestMethod]
+        public void rankingEncuestasPorValoracion()
+        {
+
+            DataTable ret = new DataTable();
+
+            
+
+            foreach (Encuesta encu in b.Encuestas())
+            {
+
+            }
+
+
+        }
+    }
+}
