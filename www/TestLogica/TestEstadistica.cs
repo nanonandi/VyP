0f08aa723748733c92ee41c51a23bf9ef28d2c69
David Cacho <dcs0028@alu.ubu.es>
2018-12-15 21:41:11 +0100

    Creación de la clase de test

---
+++ www/TestLogica/TestEstadistica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logica;
+using System.Data;
+
+namespace TestLogica {
+    [TestClass]
+    public class TestEstadistica {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
